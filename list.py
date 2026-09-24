f75199731ae149b4003da0c98b5e5e018b252b13
Fariborz Ekhtiari <fariborze7300@gmail.com>
2026-09-24 19:00:07 +0330

    Add files via upload

---
+++ list.py
@@ -0,0 +1,82 @@
+
+
+list = [1,2,"hi",[45,89,898]]
+
+#for i in list:
+#    print(i)
+
+
+#if "hi" in list:
+#    print("ok")
+
+#list_1=[]
+
+#list_2=list()   
+
+##a =12
+#
+#list_1.append(a)
+#print(list_1)
+
+
+
+# form 
+# esm va famil 
+
+
+
+# nomre migire -> py or java 
+# nomre va esm va famil va append to list_kol  
+
+#while True:
+#    list_kol=[]
+#    menu_vorod_etelaat_nomre = input("1. vorod etelaat  2. exit:")
+#    if menu_vorod_etelaat_nomre == "2":
+#        break
+#    else:
+#        while True:
+#            list_vorode_etelaat =[]
+#            match menu_vorod_etelaat_nomre:
+#                case "1":
+#                    esm = input("esmeto bezan :").capitalize()
+#                    famili =input("famili :").capitalize()
+#                    nomre_python = float(input("nomre python ro vared kon:"))
+#                    nomre_java = float(input("nomre java ra vared kon :"))
+#                    list_vorode_etelaat.append(esm)
+#                    list_vorode_etelaat.append(famili)
+#                    list_vorode_etelaat.append(nomre_python)
+#                    list_vorode_etelaat.append(nomre_java)
+#                    print(list_vorode_etelaat)
+#                    list_kol.append(list_vorode_etelaat)
+#                    print(list_kol)
+#                    break
+#                case "2":
+#                    print(list_kol)
+#                    break
+#        
+#                
+#
+#        
+            
+#list_1=[1,2,3,4]
+#m1=tuple(list_1)
+#print(m1)
+
+
+#q =(1,2,3,4)
+#m2 = list(q)
+#print(m2)
+
+#for i in q:
+#    print(i)
+
+
+
+
+import random
+
+list_1=["milad","amirali","chakavak ","fariborz",1,2,3,4]
+
+print(random.sample(list_1,2))
+
+
